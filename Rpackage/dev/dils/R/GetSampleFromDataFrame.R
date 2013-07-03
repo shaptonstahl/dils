f55df24d1464a97cc79d57e60567bf4468fab9d9
Stephen Haptonstahl <srh@haptonstahl.org>
2013-07-03 19:31:20 -0400

    Adding ScalablePCA function

---
+++ Rpackage/dev/dils/R/GetSampleFromDataFrame.R
@@ -0,0 +1,40 @@
+#' Short description of the function
+#'
+#' A longer description of the function.  This can be perhaps
+#' a paragraph, perhaps more than one.
+#' 
+#' @param n numeric, size of sample.
+#' @param x data.frame, data whose rows will be sampled.
+#' @return data.frame, size n random subset of the rows of x
+#' @export
+#' @seealso \code{\link{ScalablePCA}}
+#' @references
+#' \url{http://www.haptonstahl.org/R}
+#' @author Stephen R. Haptonstahl \email{srh@@haptonstahl.org}
+#' @examples
+#' data(iris)   # provides example data
+#' x <- Function(1, 2)     # Does something simple
+#' x                       # Display result
+#' 
+#' y <- Function(1, 2, 3)  # Does something more complicated
+#' y                       # Display result
+GetSampleFromDataFrame <- function(n,
+                                   x) {
+  # Guardians
+  if(length(n) > 1) {
+    n <- n[1]
+    warning("n has multiple values; using only the first value")
+  }
+  stopifnot(is.numeric(n),
+            n > 0,
+            0 == n %% 1)
+  stopifnot(is.data.frame(x),
+            nrow(x) > 0,
+            nrow(x) >= n)
+  
+  # perform the function
+  out <- x[sample(1:nrow(x), n),]
+  
+  # prepare and return the output
+  return(out)
+}
---
+++ Rpackage/dev/dils/R/ScalablePCA.R
@@ -0,0 +1,164 @@
+#' Perform Principal Component Analysis on a large data set
+#'
+#' Runs 'prcomp' on subsamples of the data set and compiles the
+#' results for the first dimension.
+#' 
+#' @param x data.frame, data over which to run PCA
+#' @param filename character, name of the file containing the data (NOT IMPLEMENTED)
+#' @param db Object type, database connection to table containing the data (NOT IMPLEMENTED)
+#' @param subsample numeric or logical, If an integer, size of each subsample.  If FALSE, runs PCA on entire data set.
+#' @param n.subsamples numeric, number of subsamples.
+#' @param ignore.cols numeric, indices of columns not to include
+#' @param use.cols numeric, indices of columns to use
+#' @param progress.bar logical, if TRUE then progress in running subsamples will be shown.
+#' @return vector, named vector of component weights for first dimension of principal component analysis (see example for comparison to \code{\link{prcomp}})
+#' @export
+#' @seealso \code{\link{prcomp}}
+#' @references
+#' \url{http://www.haptonstahl.org/R}
+#' @author Stephen R. Haptonstahl \email{srh@@haptonstahl.org}
+#' @examples
+#' data(iris)        # provides example data
+#' prcomp(iris[,1:4], center=FALSE, scale.=FALSE)$rotation[,1]
+#' ScalablePCA(iris, subsample=10, use.cols=1:4)
+#' ScalablePCA(iris, subsample=10, ignore.cols=5)
+ScalablePCA <- function(x,
+                        filename=NULL,
+                        db=NULL,
+                        subsample=1e4,
+                        n.subsamples=1e3,
+                        ignore.cols,
+                        use.cols,
+                        progress.bar=FALSE) {
+  # Guardians
+  if( missing(x) ) {
+    if( is.null(filename) ) {
+      if( is.null(db) ) {
+        stop("One of x, filename, or db must be specified.")
+      } else {
+        source.type <- "db"
+        # ADD CODE: TEST CONNECTION TO DATABASE
+      }
+    } else {
+      source.type <- "file"
+      if(length(filename) > 1) {
+        filename <- filename[1]
+        warning("filename has multiple values; using only the first value")
+      }
+      if( is.character(filename) ) {
+        if( !(con <- file(filename, open="rt")) ) {
+          stop("Unable to make a connection to", filename)
+        }
+      } else {
+        stop("filename must be a character (length=1) vector specifying the name of the file to open")
+      }
+    }
+  } else {
+    source.type <- "data.frame"
+    if( !is.data.frame(x) ) {
+      stop("x must be a data.frame")
+    }
+  }
+  
+  if(length(subsample) > 1) {
+    subsample <- subsample[1]
+    warning("subsample has multiple values; using only the first value")
+  }
+  if( is.logical(subsample) ) {
+    if( subsample ) {
+      subsample <- 1e4
+    } else {
+      # perform PCA on entire data set
+      n.subsamples <- 1
+    }
+  } else {
+    if( is.numeric(subsample) ) {
+      if( subsample <= 0 ) {
+        stop("subsample must be positive or FALSE")
+      }
+    } else {
+      stop("subsample must be a positive number or FALSE")
+    }
+  }
+
+  if(length(n.subsamples) > 1) {
+    n.subsamples <- n.subsamples[1]
+    warning("n.subsamples has multiple values; using only the first value")
+  }
+  if( is.numeric(n.subsamples) ) {
+    if( n.subsamples <= 0 ) {
+      stop("n.subsamples must be positive")
+    }
+  } else {
+    stop("n.subsamples must be a positive number")
+  }
+  
+  if( !missing(ignore.cols) ) {
+    ignore.cols <- sort(unique(ignore.cols))
+    if( !is.numeric(ignore.cols) ) stop("ignore.cols must be numeric")
+  }
+  if( !missing(use.cols) ) {
+    use.cols <- sort(unique(use.cols))
+    if( !is.numeric(use.cols) ) stop("use.cols must be numeric")
+  }
+  # End Guardians
+  
+  # Set internal sampling function to one of three externally-defined functions
+  if( "db" == source.type ) {
+    # n.rows.full.dataset <- ADD CODE TO READ db AND GET NUMBER OF RECORDS
+    pca.sample <- function(n=subsample) GetSampleFromDb(n=n, 
+                                                        out.of=n.rows.full.dataset, 
+                                                        db=db)
+  } else if( "file" == source.type ) {
+    # n.rows.full.dataset <- ADD CODE TO READ filename AND GET NUMBER OF LINES
+    pca.sample <- function(n=subsample) GetSampleFromFile(n=n, 
+                                                          out.of=n.rows.full.dataset, 
+                                                          con=con)
+  } else if( "data.frame" == source.type ) {
+    n.rows.full.dataset <- nrow(x)
+    pca.sample <- function(n=subsample) GetSampleFromDataFrame(n=n, 
+                                                               x=x)
+  } else{
+    stop("source.type must be one of db, file, or data.frame")
+  }
+  
+  if( missing(x) && identical(subsample, FALSE) ) subsample <- n.rows.full.dataset
+  
+  # prepare matrix to store returned weights
+  samp <- pca.sample(n=1)
+  if( !missing(ignore.cols) ) {
+    use.cols <- setdiff(1:ncol(samp), ignore.cols)
+  } else {
+    if( !all(use.cols %in% 1:ncol(samp)) ) {
+      stop("use.cols must set values between 1 and ", ncol(samp), " for this data set")
+    }
+  }
+  n.cols.used <- length(use.cols)
+  
+  if( "db" == source.type ) {
+    # ADD CODE: SET use.names TO NAMES OF COLUMNS SUBSETTED USING use.cols
+  } else if( "file" == source.type ) {
+    # ADD CODE: SET use.names TO NAMES OF COLUMNS SUBSETTED USING use.cols
+  } else if( "data.frame" == source.type ) {
+    use.names <- names(x)[use.cols]
+  } else{
+    stop("source.type must be one of db, file, or data.frame")
+  }
+  
+  draws <- matrix(0, nrow=n.subsamples, ncol=n.cols.used)
+  colnames(draws) <- use.names
+  
+  # perform the function
+  if(progress.bar) pb <- txtProgressBar(max=n.subsamples, style=2)
+  for(g in 1:n.subsamples) {
+    samp <- pca.sample()
+    pca.result <- prcomp(samp[,use.cols], center=FALSE, scale.=FALSE)
+    draws[g,] <- pca.result$rotation[,1]
+    if(progress.bar) setTxtProgressBar(pb, g)
+  }
+  if(progress.bar) close(pb)
+    
+  # prepare and return the output
+  out <- colMeans(draws)
+  return(out)
+}
